7e5de0dfbb69582b03c0c6e164893b2372c0781f
Marco Feicht <57726854+marco910@users.noreply.github.com>
2023-01-04 10:37:48 +0000

    Added image to frontpage

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,16 @@ export default function () {
   return (
     <div>
       <h1>Hello World from Next.js</h1>
-      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua.
+      </p>
+      <img
+        src="https://images.unsplash.com/photo-1672782653936-2f152a7e0a70?ixlib=rb-4.0.3&auto=format&fit=crop&w=900&q=60"
+        alt="Image"
+      />
+      <br />
       <ul>
         <li>
           <Link href="/api/test">
