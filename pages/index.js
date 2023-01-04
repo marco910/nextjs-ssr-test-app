af87c2d5b43c9017ee5641f4440be17e50f7378e
Marco Feicht <57726854+marco910@users.noreply.github.com>
2023-01-04 11:33:15 +0000

    Removed image from frontpage

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function () {
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua.
       </p>
-      <img
+      {/**<img
         src="https://images.unsplash.com/photo-1672782653936-2f152a7e0a70?ixlib=rb-4.0.3&auto=format&fit=crop&w=900&q=60"
         alt="Image"
       />
-      <br />
+      <br /> */}
       <ul>
         <li>
           <Link href="/api/test">
