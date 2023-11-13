853e9b122a289d5a2df95fc29ff315022d067ffa
Marco Feicht <57726854+marco910@users.noreply.github.com>
2023-11-13 17:09:21 +0000

    Updated front-page

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ export default function () {
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua.
       </p>
+      <p>
+        Here comes a update on the front-page...
+      </p>
       <img
         src="https://images.unsplash.com/photo-1672782653936-2f152a7e0a70?ixlib=rb-4.0.3&auto=format&fit=crop&w=900&q=60"
         alt="Image"
